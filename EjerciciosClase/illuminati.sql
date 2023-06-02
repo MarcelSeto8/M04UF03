b30b58f1a1336a1d5fdccea6500485a94ce9662e
marcel <marcel@enti>
2023-06-02 17:01:03 +0200

    aa

--- EjerciciosClase/illuminati.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS illuminati;
-
-CREATE TABLE illuminati(
-	id_illuminati INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	illuminati VARCHAR(64) NOT NULL,
-	id_user INT UNSIGNED NOT NULL
-);
