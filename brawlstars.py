218a97bb91195529080e86b3ab7e8d7c696f9229
marcel <marcel@enti>
2023-06-06 15:19:29 +0200

    Hey papu

--- brawlstars.py
+++ brawlstars.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 import requests
-from fabulous import utils, image
-import random
+from fabulous import utils,image
 
+def randomPerrito():
+	api_url = "https://dog.ceo/api/breeds/image/random"
 
-class BrawlStars():
-
-    def brawlerRandom(self):
-
-        brawler_id = random.randint(16000000, 16000069)
-
-        api_url = "https://api.brawlstars.com/v1/brawlers/"+str(pokemon_name)
-
-        response = requests.get(api_url)
+	response = requests.get(api_url)
         
-        info_brawl = (response.json())
+	info_brawl = (response.json())
+	perro = info_brawl['message']
+	
+	images = requests.get(perro).content
+
+	image_name = "perrito.png"
+
+	with open(image_name, "wb") as handler:
+		handler.write(images)
+		img = image.Image(image_name)
+		print(img)
+
+	print(info_brawl)  
         
-        id = (info_brawl["id"])
-        name = (info_brawl["name"])
-        
-        print("El id de este personaje es: " +str(id))
-        print("El nombre del personaje es: "+ name)
-       
-       
-if __name__ == "__main__":
-	app = BrawlStars()
-	app.brawlerRandom()
+
+randomPerrito()      
         
     
