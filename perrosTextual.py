02728212349be532345e67142c57a481fd664ca9
marcel <marcel@enti>
2023-06-08 18:56:15 +0200

    Actu

--- perrosTextual.py
+++ perrosTextual.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import requests
+import time
 from textual.app import App, ComposeResult
 from textual.widgets import Label, Button
 from textual import events
@@ -13,10 +14,12 @@ class HelloWorld(App):
 		self.close_button = Button("Exit", id = "close")
 		self.list_dogs = Button("List Dogs", id = "list_dogs")
 		self.next_dog = Button("Next Dog", id = "next_dog")
-		self.list_dogs_empty = Label("Empty Dogs", id = "empty_dogs")
+		self.list_dogs_empty = Label("", id = "empty_dogs")
+		self.vaciar_list_dogs = Button("Vacia Lista", id = "vaciar_list")
 
 		yield Label("Bienvenida a la tierna app de cachorritos", id="title")
 		yield self.list_dogs
+		yield self.vaciar_list_dogs
 		yield self.list_dogs_empty
 		yield self.next_dog
 		yield self.close_button
@@ -40,7 +43,17 @@ class HelloWorld(App):
 		if event.button.id == "list_dogs":
 			self.listRaces()
 			#game_dogs_list = ", ".join(list_perritos)
-			self.list_dogs_empty.update("Lista de perros "+str(list_perritos))
+			num_dogs = len(list_perritos)
+			dog_chunks = [list_perritos[i:i+5] for i in range(0, num_dogs, 5)]
+			formatted_dogs = "\n".join(", ".join(chunk) for chunk in dog_chunks)
+
+			self.list_dogs_empty.update("Lista de perros:\n "+str(formatted_dogs))
+		
+		if event.button.id == "vaciar_list":
+			self.list_dogs_empty.update("")
+		
+		if event.button.id == "close":
+			self.exit(event.button.id)
 
 if __name__ == "__main__":
 	app = HelloWorld()
